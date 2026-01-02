4bbbad17f021a69a6820a01f8381822ab75eae03
sujin9752 <tnwns9752@gmail.com>
2026-01-02 12:24:59 +0900

    12:24

--- js/intro.js
+++ js/intro.js
@@ -1,6 +1,14 @@
-window.onload = function() {
-  setTimeout(function() {
-    window.location.href = 'onboarding.html';
-  }, 2000);
-};
+window.onload = function () {
+  const intro = document.querySelector('.intro');
 
+  intro.classList.add('fade-in');
+
+  setTimeout(function () {
+    intro.classList.remove('fade-in');
+    intro.classList.add('fade-out');
+  }, 2000);
+
+  setTimeout(function () {
+    window.location.href = 'onboarding.html';
+  }, 2800); // 2000 + fade-out 0.8초
+};
